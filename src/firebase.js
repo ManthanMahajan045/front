7f93718311d81270e1362450592e30002166a4da
Harsh Sharma <harshsharma20283@gmail.com>
2026-09-17 19:52:57 +0530

    Restore Firebase config to bca4e for Google login

--- src/firebase.js
+++ src/firebase.js
@@ -4,9 +4,9 @@ import { getFirestore, collection, addDoc, doc, setDoc, getDoc, runTransaction, 
 
 const firebaseConfig = {
   apiKey: "AIzaSyDrvaJONaD-CK2_WldLkUA-NtwhFBChPkU",
-  authDomain: "road-sense-bca4.firebaseapp.com",
+  authDomain: "road-sense-bca4e.firebaseapp.com",
   projectId: "road-sense-bca4e",
-  storageBucket: "road-sense-bca4.firebasestorage.app",
+  storageBucket: "road-sense-bca4e.firebasestorage.app",
   messagingSenderId: "1032531366359",
   appId: "1:1032531366359:web:4855fc7461fbb3d4c13b92",
 };
